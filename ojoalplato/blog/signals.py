de5254edf1ff3dc70ee7469d9f71d94d47ea3156
jpalanca <jpalanca@dsic.upv.es>
2017-05-15 20:36:24 +0200

    post_save signal added.

--- ojoalplato/blog/signals.py
+++ ojoalplato/blog/signals.py
@@ -1,15 +1,15 @@
 import datetime
 from django.utils import timezone
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from .models import Post
 from .tasks import send_newsletter
 
 
 @receiver(pre_save, sender=Post)
-def post_pre_save(sender, instance, **kwargs):
-    print("Signal!!!", instance.id, instance.status)
+def blogpost_pre_save(sender, instance, **kwargs):
+    print("Pre-Signal", instance.id, instance.status)
 
     if instance.status == "publish" and not instance.notified:
         now = timezone.now()
@@ -22,5 +22,18 @@ def post_pre_save(sender, instance, **kwargs):
                 print("Sending newsletter (draft->publish)")
                 send_newsletter.apply_async((instance.id,), eta=eta)
         except Post.DoesNotExist:
-            print("Sending newsletter (post.doesnotexist)")
+            print("post.doesnotexist")
             send_newsletter.apply_async((instance.id,), eta=eta)
+
+
+@receiver(post_save, sender=Post)
+def blogpost_post_save(sender, instance, created, **kwargs):
+    print("Post-Signal", instance.id, instance.status)
+
+    if created and instance.status == "publish" and not instance.notified:
+        now = timezone.now()
+        hour = datetime.timedelta(hours=1)
+        post_date = instance.post_date
+        eta = now+hour if post_date < now+hour else post_date
+        print("Sending newsletter (post.doesnotexist)")
+        send_newsletter.apply_async((instance.id,), eta=eta)
