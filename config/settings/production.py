70bfa504db46f3b81098f765100bb6ec40368e07
jpalanca <jpalanca@gmail.com>
2017-09-19 17:59:15 +0200

    improved error messages.

--- config/settings/production.py
+++ config/settings/production.py
@@ -130,6 +130,7 @@ DEFAULT_FILE_STORAGE = 'config.settings.production.MediaRootS3BotoStorage'
 #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_MEDIA_LOCATION = 'media'
 MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
+MEDIA_ROOT = ''
 
 # Static Assets
 # ------------------------
